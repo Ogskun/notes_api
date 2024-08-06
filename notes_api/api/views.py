15f408b2eb31c3da2c1fe5087582db7e6e109ecc
Jayson Ogso <jayson.ogso@tondosolutions.com>
2024-08-06 15:26:54 +0800

    save current user

--- notes_api/api/views.py
+++ notes_api/api/views.py
@@ -55,15 +55,11 @@ class NotesViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset().filter(user=request.user)
         serializer = self.get_serializer(queryset, many=True)
-        
+
         return Response(serializer.data)
 
-    def create(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.user = request.user
-        instance.save()
-
-        return Response(status=status.HTTP_201_CREATED)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
